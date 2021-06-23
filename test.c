21956ba0f64a2415ce9732824ab4f624ad704a29
Jairo Armando <jairo.armando@gmail.com>
2021-06-23 15:07:52 -0500

    Version usando Formatos

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#define PI 3.141516171819173
+#define VALUE 36346
+
 int main(void){
-    printf("Hola Mundo!!\n");
+
+    printf("[%.2f]\n", PI );
+    printf("[%10f]\n", PI );
+    printf("[%10.2f]\n", PI );
+    printf("[%10d]\n",65535);
+    printf("[%-10d]\n",65535);
+    printf("[%010d]\n",65535);
+    printf("El Valor %d en Octal=%o y en Hexadecimal=%x\n",VALUE,VALUE,VALUE);
 
     return 0;
 }
